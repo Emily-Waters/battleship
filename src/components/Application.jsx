e9a3a3b229d260dbea15fa828beb9b651e5d4b9b
Emily <emilyabigailwaters@gmail.com>
2022-03-30 20:43:53 -0700

    Implement login form, error handling/user validation

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,20 +1,19 @@
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 import useApplicationData from "../hooks/useApplicationData";
-import useSocketMan from "../hooks/useBackend";
+import useSocketMan from "../hooks/useSocketMan";
 import "./Application.scss";
 import Login from "./Login/Login";
 import Placement from "./Placement";
-
 export default function App() {
-  const { state, shipFunctions, loginFunctions } = useApplicationData();
+  const { state, dispatch, shipFunctions, loginFunctions } = useApplicationData();
 
-  useSocketMan();
+  useSocketMan(state, dispatch);
 
   return (
     <div className="App">
       {!state.user ? (
-        <Login loginFunctions={loginFunctions} />
+        <Login loginFunctions={loginFunctions} error={state.error} />
       ) : (
         <DndProvider backend={HTML5Backend}>
           <Placement state={state} shipFunctions={shipFunctions} />
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,30 +1,51 @@
 import { useState } from "react";
 import "./Login.scss";
-
-export default function Login({ loginFunctions }) {
-  const { validateUser } = loginFunctions;
+export default function Login({ loginFunctions, error }) {
+  const { validateUser, clearErrors } = loginFunctions;
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
 
   return (
     <div className="login-container">
       <h2 className="login-title">BATTLESHIP</h2>
+      <span className="login-error" style={{ opacity: error ? 1 : 0 }}>
+        {error}
+      </span>
       <form
         className="login-form"
-        onSubmit={(e) => {
+        onSubmit={async (e) => {
           e.preventDefault();
           validateUser(name, password);
         }}
       >
         <label htmlFor="Username" className="login-form-label">
           Username&nbsp;:&nbsp;
-          <input type="text" required value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            required
+            value={name}
+            onChange={(e) => {
+              if (error) clearErrors();
+              setName(e.target.value);
+            }}
+          />
         </label>
         <label htmlFor="Password" className="login-form-label">
           Password&nbsp;&nbsp;:&nbsp;
-          <input type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input
+            type="password"
+            required
+            value={password}
+            onChange={(e) => {
+              if (error) clearErrors();
+              setPassword(e.target.value);
+            }}
+          />
         </label>
-        <button className="login-form-btn">Login</button>
+        <span className="login-btn-container">
+          <button className="login-form-btn">Register</button>
+          <button className="login-form-btn">Login</button>
+        </span>
       </form>
     </div>
   );
